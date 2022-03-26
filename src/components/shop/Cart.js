2c59628aa60ab4dd481f604ca8b882a1f0f4efd4
Sohanur <sohanur471@gmail.com>
2022-03-26 12:28:36 +0600

    fontawesome icon added

--- src/components/shop/Cart.js
+++ src/components/shop/Cart.js
@@ -36,8 +36,8 @@ const Cart = ({cart, handleResetCart}) => {
                         {cart.map((cartItem) => <li className="list-group-item text-muted" key={cartItem.id}><img className='rounded-circle border border-1 me-2' style={{height: 50, width: 50}} src={cartItem.image} alt={cartItem.name}/> {cartItem.name}</li>)}
                     </ul>
 
-                    <button className="btn btn-outline-info m-2" onClick={() => randomSelector()}>Choose 1 for me</button>
-                    
+                    <button className="btn btn-outline-success mb-2" onClick={() => randomSelector()}>Choose 1 for me</button>
+                    <br></br>
                     <button className="btn btn-outline-danger" onClick={() => handleResetCart()}>Choose again</button>
                     
                 </div>
--- src/components/shop/Product.js
+++ src/components/shop/Product.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 
 const Product = ({data, handleAddToCart}) => {
-    const {id, name, price, image} = data;
+    const {name, price, image} = data;
 
     return (
         <div className='col-md-4 col-12'>
@@ -10,7 +12,7 @@ const Product = ({data, handleAddToCart}) => {
                 <div className="card-body">
                     <h5 className="card-title">{name}</h5>
                     <p className="card-text">{price}</p>
-                    <button className="btn btn-primary" onClick={() => handleAddToCart(data)}>ADD TO CART</button>
+                    <button className="btn btn-outline-success" onClick={() => handleAddToCart(data)}>ADD TO CART <FontAwesomeIcon icon={faCartShopping} /></button>
                 </div>
             </div>
         </div>
