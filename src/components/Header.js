24d579ffb6003ddb860e2671b240d012cdde54e5
Sohanur <sohanur471@gmail.com>
2022-03-26 10:06:34 +0600

    Header component added

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Header = () => {
+    return (
+        <div className='container-fluid text-center p-5 bg-success'>
+            Fresh Grocery Store
+        </div>
+    );
+};
+export default Header;
